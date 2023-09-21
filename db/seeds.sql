a57ec0d03e6766ec02b22dfcdcfa673d77a4204c
Kayla <sullenspartyof3@gmail.com>
2023-09-21 11:53:43 -0700

    Installed inquirer & started seeds.sql & index.js

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (name)
+VALUES  (""),
+        
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Manager", 100000, 1),
+       
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Kayla", "Sullens",  1, NULL),
+        ("John", "Doe",  2, 1),
+     
