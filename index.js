893ade27b3393c5fdb6f79a95b9879804c29c484
Kayla <sullenspartyof3@gmail.com>
2023-09-25 16:17:05 -0700

    Switched if/then to switch case

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
 const {prompt} = require("inquirer");
 const db = require("./db/db.js");
 
-const init = ()  => {
-    prompt(
-        [
-            {
-                type: "list",
-                name: "task",
-                message: "What would you like to do?",
-                choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
-            }
-        ]
-    ).then(ans=>{
-        let {task} = ans;
-        if (task=="view all departments") db.getDepts().then(init);
-        if (task=="view all roles") db.getRoles().then(init);
-        if (task=="view all employees") db.getEmployees().then(init);
-
-        if (task=="add a department").then(addDepartment());
-        if (task=="add a role").then(addRole());
-        if (task=="add an employee").then(addEmployee());
-        if (task=="update an employee role").then(updateRole());
-    })
-};
+// Got help from AskBCS who shared how to correctly use a switch statement here.
+// I was using if/then before and functions.
+const init = () => {
+    prompt([
+      {
+        type: "list",
+        name: "task",
+        message: "What would you like to do?",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+        ],
+      },
+    ]).then((ans) => {
+      let { task } = ans;
+      switch (task) {
+        case "view all departments":
+          db.getDepts().then(init);
+          break;
+        case "view all roles":
+          db.getRoles().then(init);
+          break;
+        case "view all employees":
+          db.getEmployees().then(init);
+          break;
+        case "add a department":
+          addDepartment();
+          break;
+        case "add a role":
+          addRole();
+          break;
+        case "add an employee":
+          addEmployee();
+          break;
+        case "update an employee role":
+          updateRole();
+          break;
+        default:
+          console.log("Invalid task selected");
+          break;
+      }
+    });
+  };
 
 init();
 
