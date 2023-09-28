35a698cccc04b0c22e1007b17cf68da1c26d960c
Kayla <sullenspartyof3@gmail.com>
2023-09-28 13:32:50 -0700

    Fixed getRoles and getEmployees method

--- db/db.js
+++ db/db.js
@@ -13,13 +13,38 @@ class DB {
 
     getRoles = () => 
     // Do a join here
-        this.db.promise().query("SELECT * FROM role")
+        this.db.promise().query(`
+            SELECT 
+                 r.id,
+                 r.title,
+                 d.name AS department,
+                 r.salary
+            FROM role AS r
+            JOIN department AS d
+            ON r.department_id = d.id;
+        `)
             .then(([data])=> console.table(data));
             
 
     getEmployees = () => 
      // Do a join here
-        this.db.promise().query("SELECT * FROM employee")
+        this.db.promise().query(`
+            SELECT 
+                e.id,
+                e.first_name,
+                e.last_name,
+                r.title,
+                d.name AS department,
+                r.salary,
+                concat(e2.first_name, " ", e2.last_name) AS manager
+            FROM employee AS e
+            JOIN role AS r
+            ON e.role_id = r.id
+            JOIN department AS d
+            ON r.department_id = d.id
+            JOIN employee AS e2
+            ON e.manager_id = e2.id;
+            `)
             .then(([data])=> console.table(data));
             
 }
--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-const {prompt} = require("inquirer");
 const db = require("./db/db.js");
+const {prompt} = require("inquirer");
+const questions = require('./lib/questions.js');
 
 // Got help from AskBCS who shared how to correctly use a switch statement here.
 // I was using if/then before and functions.
+
 const init = () => {
-    prompt([
-      {
-        type: "list",
-        name: "task",
-        message: "What would you like to do?",
-        choices: [
-          "view all departments",
-          "view all roles",
-          "view all employees",
-          "add a department",
-          "add a role",
-          "add an employee",
-          "update an employee role",
-        ],
-      },
-    ]).then((ans) => {
+    prompt(questions).then((ans) => {
       let { task } = ans;
       switch (task) {
         case "view all departments":
@@ -52,74 +39,6 @@ const init = () => {
 
 init();
 
-function addDepartment() {
-  inquirer.prompt([
-    {
-      name: "dept",
-      type: "input",
-      message: "Please enter the department you would like to add:"
-    }
-    ]).then(function(answer) {
-}
 
-function addRole() {
-  inquirer.prompt([
-    {
-      name: "role",
-      type: "input",
-      message: "Please enter the role you would like to add:"
-    },
-    
-    {
-        name: "dept",
-        type: "list",
-        message: "Which department would you like to add this role to?",
-        choices: 
-    },
-    
-    {
-      name: "salary",
-      type: "number",
-      message: "Please enter the salary for this role:"
-    },
-    
-     ]).then(function(answer) {
-}
-
-function addEmployee() {
-  
-    inquirer.prompt([
-    {
-      name: "firstname",
-      type: "input",
-      message: "What is the employee's first name?"
-    },
-  
-    {
-        name: "lastname",
-        type: "input",
-        message: "What is the employee's last name?"
-    },
-  
-    {
-        name: "role",
-        type: "list",
-        message: "What is the employee's role?",
-        choices: 
-      },
-  
-      {
-        name: "reportingTo",
-        type: "list",
-        message: "Who is the employee's manager?",
-        choices: 
-      }
-    
-     ]).then(function(answer) {
-};
-
-function updateRole() {
-
-}
 
 //Export array into a file
